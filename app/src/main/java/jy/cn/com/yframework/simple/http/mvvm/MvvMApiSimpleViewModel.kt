93c36793badbf404b668a4db92be953463911728
958836746@qq.com <jyabc024420>
2019-11-01 18:06:52 +0800

    修改MVVM示例

--- app/src/main/java/jy/cn/com/yframework/simple/http/mvvm/MvvMApiSimpleViewModel.kt
+++ app/src/main/java/jy/cn/com/yframework/simple/http/mvvm/MvvMApiSimpleViewModel.kt
@@ -1,5 +1,6 @@
 package jy.cn.com.yframework.simple.http.mvvm
 
+import android.databinding.ObservableField
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import jy.cn.com.ylibrary.base.viewmodel.BaseViewModel
@@ -13,16 +14,19 @@ import jy.cn.com.ylibrary.util.YLogUtil
  */
 class MvvMApiSimpleViewModel : BaseViewModel<MvvMApiSimpleModel>() {
 
+    val message = ObservableField<String>()
+
     fun getBanner(showPlace: Int) {
-        YLogUtil.i("获取banner--showPlace", showPlace,mMode)
+        YLogUtil.i("获取banner--showPlace", showPlace, mMode)
 
         val disposable = mMode?.getBanner(1)!!
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ it ->
                     YLogUtil.i("获取banner--成功", it)
+                    message.set(it.toString())
                 }, {
-
+                    message.set("请求失败")
                 })
         addDisposable(disposable)
 
